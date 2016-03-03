5edda23389032d0c19ef61fcafdb88cde99685ed
samuel <samuel@DeluxxeLaptop.deluxxe.ch>
2016-03-03 22:19:13 +0100

    interfacedocumentation updated from simons fork.

--- webpy/etc/config.py
+++ webpy/etc/config.py
@@ -17,7 +17,7 @@ app_logfile="var/application.log"
 sql_logfile="var/sql.log"
 
 # session defaults
-session_salt="7c4984cd-979b-492f-8677-4bf28b527b73"
+session_salt="9222a425-76e1-4c76-b6ad-40a2410ee207"
 session_timeout=86400 #24 * 60 * 60, # 24 hours   in seconds
 session_dir='var'
 session_dir_prefix="session_"
